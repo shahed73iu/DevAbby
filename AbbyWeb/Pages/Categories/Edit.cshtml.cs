d7176db25d3b516bb269c040dfc87c3cf065cd2c
shahed73iu <shahedimamuddin@gmail.com>
2022-06-11 02:58:50 +0600

    S-03-026 Back to List

---
+++ AbbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,39 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    [BindProperties]
+    public class CategoryEditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        //[BindProperty] for a single property
+        public Category Category { get; set; }
+        public CategoryEditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int id)
+        {
+            Category = _db.Categories.Find(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "Display order can not be same as Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+
+        }
+    }
+}
